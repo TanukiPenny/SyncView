5549b899d4016aae1dcb8c6b16a09566ea06f6f8
Penny <penny@penny.moe>
2024-03-19 19:39:27 -0400

    a

--- SyncView/SVListener.cs
+++ SyncView/SVListener.cs
@@ -1,16 +1,31 @@
+using MessagePack;
 using SVCommon;
 
 namespace SyncView;
 
 public class SVListener : PacketHandler<SVClient>
 {
-    private SVClient _svClient;
-    
     public override void OnPing(SVClient conn)
     {
-        _svClient = new SVClient();
-        
         Console.WriteLine("Ping!");
         Program.SVClient.SendPing();
+    }
+
+    public override void OnBasicMessage(SVClient conn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnSerializationException(MessagePackSerializationException exception, int packetID)
+    {
+        throw new NotImplementedException();    }
+
+    public override void OnByteLengthMismatch(SVClient conn, int readBytes, int totalBytes)
+    {
+        throw new NotImplementedException();    }
+
+    public override void OnPacketHandlerException(Exception exception, int packetID)
+    {
+        throw new NotImplementedException();    
     }
 }
