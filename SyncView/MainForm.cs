ea2062cc23088a398ddf2ff3d6bb18e367823ec3
Penny <penny@penny.moe>
2024-03-21 14:16:19 -0400

    Bit more

--- SyncView/MainForm.cs
+++ SyncView/MainForm.cs
@@ -15,6 +15,6 @@ public partial class MainForm : Form
     {
         Core.Initialize();
 
-        videoView.MediaPlayer = Program.MediaManager.Player;
+        videoView.MediaPlayer = Program.MediaManager?.Player;
     }
 }
